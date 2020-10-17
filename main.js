3f4c7313dbc70a5cb228f4487962ad125baba6fe
Alexander Ignatev <alexign473@gmail.com>
2020-10-17 16:01:16 +0300

    created

---
+++ main.js
@@ -0,0 +1,16 @@
+let myNumber = +prompt('Enter the number: ')
+
+function parse(myNumber) {
+    as
+}
+
+let testNumber = 512
+
+let parsedNumber = {
+    ones: '2',
+    twos: '1',
+    threes: '5',
+
+}
+
+console.log(testNumber, parsedNumber)
